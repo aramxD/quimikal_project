e80a3270d3c895ea36bf7a6d2f1340db7f2d8903
julio santacruz <aram.jsh1@gmail.com>
2022-08-26 10:10:21 -0700

    lol

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://juliosantacruz.dev/quimikal_project/',
+  base: 'https://www.quimikal.com/',
   plugins: [react()]
 })
