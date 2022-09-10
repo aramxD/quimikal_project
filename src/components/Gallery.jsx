9b82cf891bb4dd8b756134e81149bf9c862ba91c
julio santacruz <aram.jsh1@gmail.com>
2022-09-09 17:32:05 -0700

    se agrega galeria de eventos

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -44,12 +44,12 @@ const Gallery = ({ className }) => {
           // when window width is >= 640px
           640: {
             width: 640,
-            slidesPerView: 2,
+            slidesPerView: 1,
           },
           // when window width is >= 768px
           768: {
             width: 768,
-            slidesPerView: 3,
+            slidesPerView: 2,
           },
         }}
         modules={[Navigation, Pagination, Scrollbar, A11y]}
