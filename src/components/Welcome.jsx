b8dee05f79c7c154b32ff6496ed95ecdf9409151
julio santacruz <aram.jsh1@gmail.com>
2022-10-04 12:56:21 -0700

    ajuste background hero

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -91,20 +91,21 @@ export default styled(Welcome)`
   display: flex;
   flex-direction: column;
   width: 100vw;
-  max-width: 1250px;
+  //max-width: 1250px;
   height: auto;
   padding: 0 5vw;
 
   text-align: center;
   font-style: italic;
   align-items: center;
-  margin: 0 auto;
+  //margin: 0 auto;
 
   color: black;
   background-image: url("https://images.unsplash.com/photo-1627931539006-d5c4677e05ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80");
   background-position: center; /* Center the image */
   background-repeat: no-repeat; /* Do not repeat the image */
-  background-size: cover; /* Resize the background image to cover the entire container */
+  
+  background-size: contain;
   z-index: 2;
 
   .imgHero {
@@ -123,12 +124,16 @@ export default styled(Welcome)`
     padding: 0;
     width: 100%;
   }
+
   @media (min-width: 600px) {
+    padding: 0 5vw;
+     
     img {
       width: 70%;
     }
   }
   @media (min-width: 800px) {
+    padding: 0 5vw;
     img {
       width: 40%;
     }
